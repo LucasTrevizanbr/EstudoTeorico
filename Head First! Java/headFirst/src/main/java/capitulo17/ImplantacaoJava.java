ee6a23c78d2ac6ae70771de43927bec9e2ef6d31
Lucas Trevizan <72326473+LucasTrevizanbr@users.noreply.github.com>
2021-07-11 09:20:21 -0300

    Revisão completa, os dois ultimos capitulos não me pareceram tão

---
+++ Head First! Java/headFirst/src/main/java/capitulo17/ImplantacaoJava.java
@@ -0,0 +1,18 @@
+package capitulo17;
+
+public class ImplantacaoJava {
+    /*
+        Capitulo focado em implementação de um app java finalizado, java web, app RMI e Servlets.
+            A maneira local de se implantar um aplicativo java é quando ele roda completamente na máquina
+        do usuário final, uma combinação local é quando parte roda na máquina e parte roda em um servidor e
+        remota obviamente é quando o app é totalmente executado no servidor e o cliente o acessa com algum
+        meio não relacionado a java(um browser, por exemplo).
+            Um Arquivo JAR é um Java Archive, ele permite que você empacote todas as suas classes em um único
+        arquivo JAR. Para tornar um arquivo JAR executável você precisa informar que classe tem o método main(),
+        colocando um arquivo txt com a classe no mesmo diretório do JAR, então coloca um "manifest.txt" com uma
+        linha que possua a classe com o método main "Classe-Principal: MeuApp".
+            Java web start: Os usuários finais poderão iniciar um app Java Web Start clicando em um link na web,
+         uma vez que o app tiver sido baixado ele será executado fora do navegador. Na verdade, um app Java Web
+         Start é apenas um arquivo JAR executável que é distribuído através da web
+     */
+}
---
+++ Head First! Java/headFirst/src/main/java/capitulo18/RMI.java
@@ -0,0 +1,12 @@
+package capitulo18;
+
+public class RMI {
+    /*
+        RMI(Remote Method Invocation) permite causar uma chamada em objeto remoto(que esta situado em um heap
+        em outro local), mas com um código que lhe permita *simular* que está chamando um método em um
+        objeto local.
+             Precisamos de auxiliares no lado do cliente e no lado do servidor. Eles manipularão todos os
+        detalhes de E/S e rede de nível inferior para que o cliente e o servidor possam agir como se estivessem
+        no mesmo heap.
+     */
+}
